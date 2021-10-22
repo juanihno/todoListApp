9e6f9fc88864acaa0ba5b68dcc1563ae9f7cd08c
Juan <jjjbaque@gmail.com>
2021-10-23 10:55:44 +1100

    saving data yeahhhh

--- App.js
+++ App.js
@@ -1,29 +1,65 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
 import TaskInputField from './components/TaskInputField';
 import TaskItem from './components/TaskItem';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 export default function App() {
   const [tasks, setTasks] = useState([]);
+  const [ appInit, setAppInit ] = useState( true )
+
+
+  useEffect( () => {
+    if( appInit ) {
+      getData()
+      setAppInit( false )
+      console.log('getting data...')
+    }
+    else {
+      storeData()
+      console.log('storing data...')
+    }
+    // sortData()
+  }, [tasks] )
+
 
   const addTask = (task) => {
     if (task == null) return;
     setTasks([...tasks, task]);
+
     Keyboard.dismiss();
 
   }
 
   const deleteTask = (deleteIndex) => {
-    setTasks(tasks.filter((value, index) => index != deleteIndex));
+    setTasks(tasks.filter((_value, index) => index != deleteIndex));
   }
+  const storeData = async () => {
+    const stringified = JSON.stringify( tasks )
+    try {
+      await AsyncStorage.setItem( "listData" , stringified ) 
+    } catch (error) {
+      console.log( error )
+    }
+  }
+
+  const getData = async () => {
+    try {
+      const stringified = await AsyncStorage.getItem("listData")
+      setTasks( (stringified !== null) ? JSON.parse(stringified) : [] )
+    } catch (error) {
+      console.log( error )
+    }
+  }
+
 
   return (
     <View style={styles.container}>
         <Text style={styles.heading}>TODO LIST</Text>
       <ScrollView style={styles.scrollView}>
         {
-        tasks.map((task, index) => {
+        tasks.map(( task, index) => {
           return (
             <View key={index} style={styles.taskContainer}>
               <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
